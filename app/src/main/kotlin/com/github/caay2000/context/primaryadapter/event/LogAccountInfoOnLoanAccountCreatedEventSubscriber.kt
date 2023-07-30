cb42e20c9756cca56ef8ea69ce30c928c1d42811
Albert Casanovas <caay2000@gmail.com>
2023-07-31 00:33:11 +0200

    Cleaning template (#1)

---
+++ app/src/main/kotlin/com/github/caay2000/context/primaryadapter/event/LogAccountInfoOnLoanAccountCreatedEventSubscriber.kt
@@ -0,0 +1,19 @@
+package com.github.caay2000.context.primaryadapter.event
+
+import com.github.caay2000.common.event.DomainEventSubscriber
+import com.github.caay2000.context.application.AccountRepository
+import com.github.caay2000.context.application.print.LogAccountInfoCommand
+import com.github.caay2000.context.application.print.LogAccountInfoCommandHandler
+import com.github.caay2000.events.AccountCreatedEvent
+import mu.KLogger
+import mu.KotlinLogging
+
+class LogAccountInfoOnLoanAccountCreatedEventSubscriber(accountRepository: AccountRepository) : DomainEventSubscriber<AccountCreatedEvent>() {
+
+    override val logger: KLogger = KotlinLogging.logger {}
+    private val commandHandler = LogAccountInfoCommandHandler(accountRepository)
+
+    override fun handleEvent(event: AccountCreatedEvent) {
+        commandHandler.handle(LogAccountInfoCommand(event.id))
+    }
+}
---
+++ app/src/test/kotlin/com/github/caay2000/common/TestUseCases.kt
@@ -0,0 +1,52 @@
+package com.github.caay2000.common
+
+import com.github.caay2000.common.test.http.HttpDataResponse
+import com.github.caay2000.common.test.mock.MockDateProvider
+import com.github.caay2000.common.test.mock.MockIdGenerator
+import com.github.caay2000.context.domain.Account
+import com.github.caay2000.context.domain.AccountId
+import com.github.caay2000.context.domain.Email
+import com.github.caay2000.context.domain.IdentityNumber
+import com.github.caay2000.context.domain.PhoneNumber
+import com.github.caay2000.context.domain.PhonePrefix
+import com.github.caay2000.context.mother.AccountMother
+import com.github.caay2000.context.primaryadapter.http.serialization.AccountDetailsDocument
+import io.ktor.server.testing.ApplicationTestBuilder
+import java.time.LocalDateTime
+import java.util.UUID
+
+class TestUseCases(
+    private val libraryClient: LibraryClient = LibraryClient(),
+    private val mockIdGenerator: MockIdGenerator? = null,
+    private val mockDateProvider: MockDateProvider? = null,
+) {
+
+    context(ApplicationTestBuilder)
+    fun `account is created`(
+        account: Account = AccountMother.random(),
+        accountId: AccountId? = null,
+        identityNumber: IdentityNumber? = null,
+        email: Email? = null,
+        phonePrefix: PhonePrefix? = null,
+        phoneNumber: PhoneNumber? = null,
+    ): HttpDataResponse<AccountDetailsDocument> {
+        val id = accountId?.value ?: account.id.value
+        `id will be mocked`(id)
+        `datetime will be mocked`(account.registerDate.value)
+        return libraryClient.createAccount(
+            identityNumber = identityNumber ?: account.identityNumber,
+            name = account.name,
+            surname = account.surname,
+            birthdate = account.birthdate,
+            email = email ?: account.email,
+            phonePrefix = phonePrefix ?: account.phonePrefix,
+            phoneNumber = phoneNumber ?: account.phoneNumber,
+        )
+    }
+
+    context(ApplicationTestBuilder)
+    fun `find account`(id: AccountId): HttpDataResponse<AccountDetailsDocument> = libraryClient.findAccount(id)
+
+    private fun `id will be mocked`(id: UUID): UUID = mockIdGenerator?.mock(id).let { id }
+    private fun `datetime will be mocked`(datetime: LocalDateTime): LocalDateTime = mockDateProvider?.mock(datetime).let { datetime }
+}
